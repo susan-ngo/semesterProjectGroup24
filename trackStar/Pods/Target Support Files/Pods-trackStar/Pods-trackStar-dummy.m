133cd8acbdb01e24579706f433c7a43bfd4d7a30
User <user@PhoneHeros-MacBook-Pro.local>
2019-04-24 23:37:04 -0400

    Google Maps SDK added

---
+++ trackStar/Pods/Target Support Files/Pods-trackStar/Pods-trackStar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_trackStar : NSObject
+@end
+@implementation PodsDummy_Pods_trackStar
+@end
--- trackStar/trackStar/RidesController.swift
+++ trackStar/trackStar/RidesController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import CoreData
 
-class RidesController: UIViewController {
+class RidesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var rides: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,19 @@ class RidesController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rides.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "standard", for: indexPath)
+        cell.textLabel?.text = rides[indexPath.row]
+        return cell
+    }
 
     /*
     // MARK: - Navigation
